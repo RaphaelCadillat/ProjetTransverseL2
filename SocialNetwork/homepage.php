9f556478058bb4709ad478c3d2623de869f988fa
Sl33pyCl0ud <julien.hassoun@efrei.net>
2021-03-20 15:39:47 +0100

    test 2

--- SocialNetwork/homepage.php
+++ SocialNetwork/homepage.php
@@ -12,17 +12,3 @@
     </body>
 </html>
 <!-- Est ce que git marche vraiment ?-->
-=======
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset = "utf-8" />
-        <title>Social Network</title>
-        
-    </head>
-
-    <body>
-        <p>Vous etes sur la homepage</p>
-    </body>
-</html>
->>>>>>> bdf80b8bb4db43bce3e1c72a1dc09af9e454007b:SocialNetwork/homepage.php

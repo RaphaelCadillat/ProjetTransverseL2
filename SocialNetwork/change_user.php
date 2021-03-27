769a7f533704729f4ad62bbec5a1eba0be9c721e
Sl33pyCl0ud <julien.hassoun@efrei.net>
2021-03-28 00:53:27 +0100

    barre de menu

--- SocialNetwork/change_user.php
+++ SocialNetwork/change_user.php
@@ -2,6 +2,18 @@
 <html>
     <head>
         <meta charset = "utf-8" />
+        <link rel="stylesheet" href="Style.css" />
+        <div>
+        <button id="profile" name="profile" type="submit" formaction="profile.php"> Profile </button>
+        </div>
+        <div>
+        <button id="logout" name="log_out" type="submit" formaction="logout.php"> Log Out </button>
+        </div>
+        <div>
+        <button id="friends" name="friends" type="submit" formaction="friends.php"> Friends </button>
+        </div>
+        <br>
+        <br>
         <title>Social Network</title>
         
     </head>
---
+++ SocialNetwork/headband.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8" />
+        <title>Social Network</title>
+        
+    </head>
+
+    <body>
+        <p>Vous etes sur la homepage</p>
+    </body>
+</html>

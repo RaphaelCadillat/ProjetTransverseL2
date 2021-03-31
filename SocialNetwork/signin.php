e61c046619be7d2f733c35c6f757f36a903d73b5
RaphaelCadillat <73220774+RaphaelCadillat@users.noreply.github.com>
2021-03-31 10:37:31 +0200

    Dev signin design  en cours
    Début du développement design de la page de connexion.

--- SocialNetwork/signin.php
+++ SocialNetwork/signin.php
@@ -3,18 +3,20 @@
     <head>
         <meta charset = "utf-8" />
         <title>Social Network</title>
-        
+        <link rel="stylesheet"href="signin.css" />
     </head>
     <body>
         <p>Vous etes sur la page d'identification !</p>
-
+        <div class="formulaire">
         <form action="" method="post">
-
+       <strong> Email : </strong><br>
             <div>
+            
             <input id="email" type="email" name="mail_user" placeholder="Email" required="required">
             </div><br>
-
+            <strong>Password :</strong><br>
             <div>
+            
             <input id="password" type="password" name="pass_user" placeholder="Password" required="required">
             </div><br>
 
@@ -24,6 +26,7 @@
             <?php include("verify_log_user.php"); ?>
 
         </form>
+        </div>
 
     </body>
 </html>

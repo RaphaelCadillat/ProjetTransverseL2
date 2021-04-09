a57c1bc00f67460914e168673d82d93470e0bc85
Sl33pyCl0ud <julien.hassoun@efrei.net>
2021-04-09 17:25:10 +0200

    log out last

--- SocialNetwork/Model/logout.php
+++ SocialNetwork/Model/logout.php
@@ -1,9 +1,5 @@
 <?php
 require 'session_util.php';
-
-if (isset($_POST['logout']))
-{
-    free_php_session();
-    header("Location: ../../index.php");
-    //echo "<script>window.open('../View/Pages/signup.php','_self')</script>";
-}
+free_php_session();
+header("Location: ../index.php");
+//echo "<script>window.open('../View/Pages/signup.php','_self')</script>";
--- SocialNetwork/View/Pages/navigation_bar.php
+++ SocialNetwork/View/Pages/navigation_bar.php
@@ -3,13 +3,7 @@
         <li><a href="homepage.php">Homepage</a></li>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="friends.php">Friends</a></li>
-        <li><a href="friends.php">Friends</a></li>
+        <li><a href="../../Model/logout.php">Log out</a></li>
     </ul>
 </nav>
 
-<!--
-<form action="" method="post">
-            <button name="logout" id="logout" action="">Log out</button><br>
-            <?php /* include ("logout.php"); */?>
-        </form>
--->

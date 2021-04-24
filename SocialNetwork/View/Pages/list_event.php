462792a92c4873c6847e6cda2596d471211001a5
Sl33pyCl0ud <julien.hassoun@efrei.net>
2021-04-25 00:15:03 +0200

    creation event

---
+++ SocialNetwork/View/Pages/list_event.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8" />
+        <link rel="stylesheet" href="../Styles/Style.css" />
+        <link rel="stylesheet" href="../Styles/events.css" />
+        <title>Social Network</title>
+        
+    </head>
+    <body>
+        <?php include('navigation_bar.php') ?>
+        <nav id="eventnav">
+            <ul id="enav">
+                <li><a href="create_event.php">Créer un event</a></li>
+                <li><a href="list_event.php">Tous les events</a></li>
+                <li><a href="friends_event.php">Rechercher un event d'un ami</a></li>
+            </ul>
+        </nav>
+        <br>
+        <form action="" method="post">
+
+            <div>
+            <p>Event Name :</p>
+            <input id="eventname" name="event_name" value="" disabled>
+            </div>
+            <br>
+
+            <div>
+            <p>Description :</p>
+            <br>
+            <textarea id="descriptionevent" name="description_event" disabled></textarea>
+            </div>
+            <br>
+
+    </body>
+</html>
--- SocialNetwork/View/Pages/navigation_bar.php
+++ SocialNetwork/View/Pages/navigation_bar.php
@@ -2,6 +2,7 @@
     <ul id="menu">
         <li><a href="homepage.php">Homepage</a></li>
         <li><a href="profile.php">Profile</a></li>
+        <li><a href="create_event.php">Events</a></li>
         <li><a href="friends.php">Friends</a></li>
         <li><a href="../../Model/logout.php">Log out</a></li>
     </ul>
